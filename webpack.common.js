8790848c0eb556ead03421a1c8d1e26f37ee08c0
Hanqiu Jiang <hanq.jiang@gmail.com>
2020-04-11 21:17:28 +0200

    feat: Setup minimal webpack config

---
+++ webpack.common.js
@@ -0,0 +1,15 @@
+const path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/client/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, './src/client/views/index.html')
+    })
+  ]
+};
