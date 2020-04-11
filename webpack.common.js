9e194f135ad8b9e3ebd15969ff7d1e8146ab4201
Hanqiu Jiang <hanq.jiang@gmail.com>
2020-04-11 21:38:43 +0200

    feat: Add devServer

--- webpack.common.js
+++ webpack.common.js
@@ -2,7 +2,7 @@ const path = require('path');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: './src/client/index.js',
+  entry: path.resolve(__dirname, './src/client/index.js'),
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js'
