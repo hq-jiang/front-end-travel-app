cc92175fd810b5e70dc2b3b856b72f0a3232578b
Hanqiu Jiang <hanq.jiang@gmail.com>
2020-04-13 13:36:39 +0200

    feat: Add sass loader with empty styles

--- webpack.common.js
+++ webpack.common.js
@@ -7,6 +7,21 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js'
   },
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          'style-loader',
+          // Translates CSS into CommonJS
+          'css-loader',
+          // Compiles Sass to CSS
+          'sass-loader',
+        ],
+      },
+    ],
+  },
   plugins: [
     new HtmlWebpackPlugin({
       template: path.resolve(__dirname, './src/client/views/index.html')
