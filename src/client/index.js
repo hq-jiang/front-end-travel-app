8aec64fd37c826332901b92cafcac3db06167c57
Hanqiu Jiang <hanq.jiang@gmail.com>
2020-04-15 00:50:11 +0200

    feat: Add example pixabay image dynamically

--- src/client/index.js
+++ src/client/index.js
@@ -1,3 +1,7 @@
 import './styles/style.scss';
 
 // alert('Hello World');
+const imgResource = 'https://pixabay.com/get/55e0d340485aa814f1dc84609629317e153fd7e6564c704c7d2972d4944ecc5e_640.jpg';
+
+const resultImageDiv = document.getElementById('result-image');
+resultImageDiv.innerHTML = `<img src="${imgResource}" height="auto" width="100%">`;
