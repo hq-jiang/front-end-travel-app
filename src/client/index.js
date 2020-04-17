cba7b94babca81fca9a04b2b2fd12ee2234434ce
Hanqiu Jiang <hanq.jiang@gmail.com>
2020-04-17 16:50:53 +0200

    feat: Fix behavior upon enter click

--- src/client/index.js
+++ src/client/index.js
@@ -7,3 +7,18 @@ setCalendarMinDate();
 // Listen to submit action
 const submitButton = document.getElementById('submit-button');
 submitButton.addEventListener('click', submitInputData);
+
+const formElement = document.getElementById('form');
+formElement.addEventListener('submit', (event) => {
+  event.preventDefault();
+});
+
+formElement.addEventListener('keyup', function(event) {
+  // Number 13 is the "Enter" key on the keyboard
+  if (event.keyCode === 13) {
+    // Cancel the default action, if needed
+    event.preventDefault();
+    // Trigger the button element with a click
+    submitButton.click();
+  }
+});
